b144758d336614c3000d29e9f47d20b70d8def42
01224349901Anh <maituananh>
2019-10-11 17:43:51 +0700

    update 11/10/2019

---
+++ java/exercise OOP-JAVA + COLLECTION/baitap3.cpp
@@ -0,0 +1,17 @@
+﻿Đề :
+Một công ty được giao nhiệm vụ quản lý các phương tiện giao thông gồm các loại: 
+ô tô, xe máy, xe tải.
+
++ mỗi loại phương tiện giao thông cần quản lý: Hãng sản xuất, năm sản xuất, giá bán và màu.
+
++ Các ô tô cần quản lý: số chỗ ngồi, kiểu động cơ
+
++ Xe máy cần quản lý: công suất
+
++ Xe tải cần quản lý: trọng tải.
+
+2. Xây dựng các lớp XeTai, XeMay, OTo kế thừa từ lớp PTGT.
+3. Xây dựng các hàm để truy nhập (get), thay đổi (set) và hiển thị các thuộc tính của các lớp.
+4. Xây dựng lớp QLPTGT cài đặt các phương thức thực hiện các chức năng sau:
+- Nhập đăng ký phương tiện
+- Tìm phương tiện theo màu hoặc năm sản xuất
